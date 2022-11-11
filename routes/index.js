5b1da4c56f9de90cd342b42dcef79d68caa0a073
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-11 22:43:15 +0700

    index routes

---
+++ routes/index.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+import userRoutes from "./userRoutes.js";
+import { authentication } from "../middlewares/authentication.js";
+
+const router = Router();
+
+// make route
+router.get("/", (req, res) => {
+  res.send("Hactive8 - Final Project 2");
+});
+
+// create same-endpoint
+router.use("/users", userRoutes);
+router.use("/categories", authentication, () => {});
+router.use("/products", authentication, () => {});
+router.use("/transactions", authentication, () => {});
+
+export default router;
