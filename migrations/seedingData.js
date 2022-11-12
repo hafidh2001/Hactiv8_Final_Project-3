3201d2ea768df4bd440651a97fa6c1567edd3e46
hafidh2001 <hafidhfauzan2@gmail.com>
2022-11-12 22:51:31 +0700

    fixing seeding

--- migrations/seedingData.js
+++ migrations/seedingData.js
@@ -5,33 +5,41 @@ const hashSync = (input) => {
   return bcrypt.hashSync(input, 10);
 };
 
-await db
-  .query(
-    `INSERT INTO users
-(
-    full_name,
-    email,
-    password,
-    gender,
-    role,
-    balance,
-    createdat,
-    updatedat
-)
-VALUES
-(
-    'admin haped',
-    'admin_haped@gmail.com',
-    '${hashSync("admin12345")}',
-    'male',
-    'admin',
-    100000000,
-    '${new Date().toISOString()}',
-    '${new Date().toISOString()}'
-);`
+try {
+  await db
+    .query(
+      `INSERT INTO users
+  (
+      full_name,
+      email,
+      password,
+      gender,
+      role,
+      balance,
+      createdat,
+      updatedat
   )
-  .then((res) => {
-    console.log(res);
+  VALUES
+  (
+      'admin haped',
+      'admin_haped@gmail.com',
+      '${hashSync("admin12345")}',
+      'male',
+      'admin',
+      100000000,
+      '${new Date().toISOString()}',
+      '${new Date().toISOString()}'
+  );`
+    )
+    .then((res) => {
+      console.log(res);
+    });
+} catch (e) {
+  console.log({
+    status: "error",
+    field: e.errors[0].path,
+    value: e.errors[0].value,
+    message: e.errors[0].message,
   });
-
+}
 await db.close();
